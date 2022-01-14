42efbfee5ca006e84f429cf1c122e2722f3e3180
chickensalt <nsdj.sharma@gmail.com>
2022-01-14 12:31:28 +1100

    auto-refresh after cooldown elapsed

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { readdirSync } from "fs";
 import { Command } from "./structures/command";
 import { db } from "./structures/db";
 import { cooldowns } from "./structures/cooldowns";
-import { clampSlowmode, getMpm } from "./lib";
+import { clampSlowmode, getMpm, messageCreateEvent } from "./lib";
 dotenv();
 
 const client = new Client({
@@ -35,32 +35,6 @@ client.on("interactionCreate", (interaction) => {
   command.execute(interaction);
 });
 
-client.on("messageCreate", async (message) => {
-  if (message.author.bot) return;
-  if (message.channel.type !== "GUILD_TEXT") return;
-  if (cooldowns.has(message.channelId)) return;
-
-  cooldowns.add(message.channelId);
-  setTimeout(() => {
-    cooldowns.delete(message.channelId);
-  }, 5000);
-
-  const rules = await db.rule.findMany({
-    where: {
-      channelId: message.channelId,
-    },
-  });
-  if (!rules.length) return;
-
-  const mpm = getMpm(message.channel);
-  const targetRule = rules.reduce((highest, current) =>
-    current.threshold >= mpm && current.threshold > highest.threshold
-      ? current
-      : highest
-  );
-  const targetSlowmode =
-    mpm > targetRule.threshold ? clampSlowmode(targetRule.slowmode) : 0;
-  await message.channel.setRateLimitPerUser(targetSlowmode);
-});
+client.on("messageCreate", messageCreateEvent);
 
 client.login(process.env.BOT_TOKEN);
--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,6 @@
-import { TextChannel } from "discord.js";
+import { Message, TextChannel } from "discord.js";
+import { cooldowns } from "./structures/cooldowns";
+import { db } from "./structures/db";
 
 export const getMpm = (channel: TextChannel) =>
   channel.messages.cache
@@ -9,3 +11,32 @@ export const clamp = (num: number, min: number, max: number) =>
   Math.max(min, Math.min(num, max));
 
 export const clampSlowmode = (slowmode: number) => clamp(slowmode, 0, 21600);
+
+export const messageCreateEvent = async (message: Message) => {
+  if (message.author.bot) return;
+  if (message.channel.type !== "GUILD_TEXT") return;
+  if (cooldowns.has(message.channelId)) return;
+
+  cooldowns.add(message.channelId);
+  setTimeout(() => {
+    cooldowns.delete(message.channelId);
+    messageCreateEvent(message);
+  }, 5000);
+
+  const rules = await db.rule.findMany({
+    where: {
+      channelId: message.channelId,
+    },
+  });
+  if (!rules.length) return;
+
+  const mpm = getMpm(message.channel);
+  const targetRule = rules.reduce((highest, current) =>
+    current.threshold >= mpm && current.threshold > highest.threshold
+      ? current
+      : highest
+  );
+  const targetSlowmode =
+    mpm > targetRule.threshold ? clampSlowmode(targetRule.slowmode) : 0;
+  await message.channel.setRateLimitPerUser(targetSlowmode);
+};
